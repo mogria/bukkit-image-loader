2e84f66a36c3255d51e6698f19cb06edfab3c99e
Mogria <m0gr14@gmail.com>
2012-09-21 20:59:56 +0200

    imageloader can now load images and convert it into minecraft blocks

--- src/org/cerealshit/ImageLoader/ImageLoaderCommandExecutor.java
+++ src/org/cerealshit/ImageLoader/ImageLoaderCommandExecutor.java
@@ -1,14 +1,18 @@
 
 package org.cerealshit.ImageLoader;
 
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.imageio.ImageIO;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.block.*;
-import org.bukkit.block.Block;
-import org.bukkit.Location;
-import java.util.*;
-
+import org.bukkit.entity.Player;
 
 public class ImageLoaderCommandExecutor implements CommandExecutor {
     private ImageLoader plugin;
@@ -20,7 +24,71 @@ public class ImageLoaderCommandExecutor implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String args[]) {
         sender.sendMessage("you just executed this the /" + cmd.getName() + " of the image Loader plugin :)");
-        return false;
+        if(sender instanceof Player) {
+            if(args.length >= 1) {
+                BufferedImage img = null;
+                try {
+                    img = ImageIO.read(new File(args[0]));
+
+                    loadImage(((Player)sender).getLocation(), convertImageToBlockIDs(img));
+
+                } catch(IOException ex) {             
+                }
+            } else {
+                return false;
+            }
+        } else {
+            sender.sendMessage("you con't do this on the console");
+        }
+        return true;
+    }
+    
+    public ArrayList<ArrayList<Integer>> convertImageToBlockIDs(BufferedImage img) {
+        ArrayList<ArrayList<Integer>> blockidmap = new ArrayList<ArrayList<Integer>>();
+                
+        Color[] block_colors = {
+            new Color(0xfcfcfc), //white
+            new Color(0xe6792d), //orange
+            new Color(0xc153cb), //lila 
+            new Color(0x698bd2), //cyan
+            new Color(0xd9cc28), //yellow
+            new Color(0x36ab2b), //green
+            new Color(0xd68098), //pink
+            new Color(0x454545), //darkgrey
+            new Color(0xa1a1a1), //grey
+            new Color(0x287898), //blue
+            new Color(0x893ecc), //violet
+            new Color(0x27339a), //darkblue
+            new Color(0x59351d), //brown
+            new Color(0x3b521a), //darkgreen
+            new Color(0xa72d29), //red
+            new Color(0x171717)  // black
+        };
+
+        for(int y = 0; y < img.getHeight(); y++) {
+            ArrayList<Integer> row = new ArrayList<Integer>();
+            for(int x = 0; x < img.getWidth(); x++) {
+                row.add(getIdOfClosestColor(block_colors, new Color(img.getRGB(x, y))));
+            }
+            blockidmap.add(row);
+        }
+        return blockidmap;
+    }
+    
+    public int getIdOfClosestColor(Color[] colors, Color compare) {
+        int mindiff = 0x4ff;
+        int currentdiff = 0;
+        int id = 0;
+        for(int i = 0; i < colors.length; i++) {
+            currentdiff = Math.abs(colors[i].getBlue() - compare.getBlue())
+                    + Math.abs(colors[i].getGreen() - compare.getGreen())
+                    + Math.abs(colors[i].getRed() - compare.getRed());
+            if(currentdiff < mindiff) {
+                mindiff = currentdiff;
+                id = i;
+            }
+        }
+        return id;
     }
     
     public void loadImage(Location location,ArrayList<ArrayList<Integer>> blockIds){
@@ -38,12 +106,9 @@ public class ImageLoaderCommandExecutor implements CommandExecutor {
             {
                 setLocation.setX(setLocation.getX() + 1);
                 Block bleockli = setLocation.getBlock();
-                bleockli.setTypeId(blockId);
-                
+                bleockli.setTypeIdAndData(0x23, (byte)((int)blockId), false);
             }
         }
             
     }
-
-
 }
